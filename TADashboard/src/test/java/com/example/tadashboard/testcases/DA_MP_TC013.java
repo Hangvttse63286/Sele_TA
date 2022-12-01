60551d73c40dee70c7acb90906d631bb891331b3
boykid95 <ngocthanh220295@gmail.com>
2022-12-01 20:03:04 +0700

    Test add new branch DA_MP_TC013

---
+++ TADashboard/src/test/java/com/example/tadashboard/testcases/DA_MP_TC013.java
@@ -0,0 +1,9 @@
+package com.example.tadashboard.testcases;
+
+import com.example.tadashboard.common.utilities.listener.ReportListener;
+import org.testng.annotations.Listeners;
+
+@Listeners(ReportListener.class)
+public class DA_MP_TC013 {
+
+}
